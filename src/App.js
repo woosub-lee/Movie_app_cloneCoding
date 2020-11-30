44e549356a5aedf4a374e14ed29cf9a1349c325d
woosub-lee <woosub.project@gmail.com>
2020-11-30 22:24:18 +0900

    print the movie data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state = {
@@ -11,7 +12,7 @@ class App extends React.Component{
       data: {
         data: {movies},
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({movies, isLoading: false});
   }
   componentDidMount(){
@@ -19,9 +20,27 @@ class App extends React.Component{
     this.getMovie();
   }
   render(){
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return(
-    <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
+    <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">'Loading...'</span> 
+        </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie)=> (
+              <Movie
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
