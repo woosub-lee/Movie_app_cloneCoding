271ce5951977561d88f07a6ec3476b27e8ff10d2
woosub-lee <woosub.project@gmail.com>
2021-01-03 23:12:24 +0900

    cloneCoding finish

---
+++ src/components/Movie.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './Movie.css'
+
+function Movie({title,year,summary,poster,genres}){
+    return (
+        <div className="movie">
+            <Link 
+                to={{
+                    pathname: '/movie-detail',
+                    state: {year, title, summary, poster, genres}
+                }}
+            >
+                <img src={poster} alt={title} title={title}/>
+                <div className="movie__data">
+                    <h3 className="movie__title">{title}</h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) => {
+                            return( 
+                                <li key={index} className="movie__genre">
+                                    {genre}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    {summary.length > 180 ? (
+                        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                    ) : (
+                        <p className="movie__summary">{summary}</p>
+                    )}
+                </div>
+            </Link>
+        </div>
+    );
+}
+Movie.propTypes = {
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                About this page: Movie app cloneCoding with Nomad Coder
+            </span>
+            <span>
+                Thanks to Nicolas
+            </span>
+        </div>
+    );
+}
+export default About;
